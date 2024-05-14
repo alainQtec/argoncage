11caf63ffc31cbceeaf7f354a69c01616552e3a9
Alain Herve <alainQtec@gmail.com>
2024-05-14 23:51:52 +0200

    Update TaskMan.psm1

--- Private/TaskMan/TaskMan.psm1
+++ Private/TaskMan/TaskMan.psm1
@@ -1,17 +1,8 @@
 ﻿# A small process managment class
 class TaskMan {
-    static [System.Management.Automation.PowerShell] RunInBackground([ScriptBlock]$ScriptBlock, [Object[]]$ArgumentList) {
-        $powershell = [System.Management.Automation.PowerShell]::Create()
-        $powershell.AddScript($ScriptBlock).AddParameters($ArgumentList)
-        $runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspace()
-        $runspace.Open()
-        $powershell.Runspace = $runspace
-        $powershell.BeginInvoke()
-        return $powershell
-    }
     static [System.Management.Automation.PSObject[]] RunInParallel([System.Management.Automation.Job[]]$jobs) {
         $threadjobs = $jobs | ForEach-Object { Start-ThreadJob -ScriptBlock ([ScriptBlock]::Create($_.Command)) }
-        $results = $threadjobs | Receive-Job -Wait -AutoRemoveJob
+        $results = $threadjobs | Receive-Job -Wait
         return $results
     }
     static [TaskResult] WaitTask([string]$progressMsg, [scriptblock]$scriptBlock) {
@@ -36,11 +27,16 @@ class TaskMan {
             }
         ).Error;
         if ($Job.JobStateInfo.State -eq "Failed" -or $Errors.Count -gt 0) {
-            $errormessages = [string]::Empty
+            $errormessages = ""; $errStackTrace = ""
             if ($null -ne $Errors) {
                 $errormessages = $Errors.Exception.Message -join "`n"
+                $errStackTrace = $Errors.ScriptStackTrace
+                if ($null -ne $Errors.Exception.InnerException) {
+                    $errStackTrace += "`n`t"
+                    $errStackTrace += $Errors.Exception.InnerException.StackTrace
+                }
             }
-            [taskman]::WriteLog("Completed with errors.`n`t$errormessages", $false)
+            [taskman]::WriteLog("Completed with errors.`n`t$errormessages`n`t$errStackTrace", $false)
         } else {
             [TaskMan]::WriteLog("Done.", $true)
         }
@@ -173,7 +169,7 @@ class TaskResult {
         $this.Command = $job.Command
         $get_state = [scriptblock]::Create("return '$($job.JobStateInfo.State.ToString())'")
         $this.psobject.Properties.Add([psscriptproperty]::new('State', $get_state, { throw [System.InvalidOperationException]::new("Cannot set State") }))
-        $this.Output = $job.ChildJobs | Receive-Job -Wait -AutoRemoveJob
+        $this.Output = $job.ChildJobs | Receive-Job -Wait
         # [scriptBlock]::Create("$($job.ChildJobs.Command)").Invoke()
         $this.IsSuccess = $job.JobStateInfo.State -eq "Completed"
         if (!$this.IsSuccess) {
@@ -256,7 +252,7 @@ function Invoke-RetriableCommand {
 
 function Wait-Task {
     [CmdletBinding(DefaultParameterSetName = 'ScriptBlock')]
-    [OutputType([TaskResult])]
+    [OutputType([TaskResult])][Alias('await')]
     param (
         [Parameter(Mandatory = $true, Position = 0, ParameterSetName = '__AllparameterSets')]
         [Alias('m')]
@@ -267,19 +263,96 @@ function Wait-Task {
         [scriptblock]$scriptBlock,
 
         [Parameter(Mandatory = $true, Position = 1, ParameterSetName = 'job')]
-        [System.Management.Automation.Job]$Job
+        [System.Management.Automation.Job]$Job,
+
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1, ParameterSetName = 'task')]
+        [System.Threading.Tasks.Task[]]$Task
     )
     begin {
         $result = $null
+        # $Tasks = @()
     }
     process {
         if ($PSCmdlet.ParameterSetName -eq 'scriptBlock') {
             $result = [TaskMan]::WaitTask($progressMsg, $scriptBlock)
         }
+
+        # $Tasks += $Task
+        # While (-not [System.Threading.Tasks.Task]::WaitAll($Tasks, 200)) {}
+        # $Tasks.ForEach( { $_.GetAwaiter().GetResult() })
     }
     end {
         return $result
     }
 }
+
+function New-Task {
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    [Alias('Create-Task')]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [scriptblock]$ScriptBlock,
+
+        [Parameter(Mandatory = $false, Position = 1)]
+        [Object[]]$ArgumentList
+    )
+    begin {
+        $_result = $null
+        $powershell = [System.Management.Automation.PowerShell]::Create()
+    }
+    process {
+        $_Action = $(if ($PSCmdlet.MyInvocation.BoundParameters.ContainsKey('ArgumentList')) {
+                { Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList } -as [System.Action]
+            } else {
+                { Invoke-Command -ScriptBlock $ScriptBlock } -as [System.Action]
+            }
+        )
+        $powershell.AddScript({
+                param (
+                    [Parameter(Mandatory)]
+                    [ValidateNotNull()]
+                    [System.Action]$Action
+                )
+                return [System.Threading.Tasks.Task]::Factory.StartNew($Action)
+            }
+        ).AddArgument($_Action)
+        $runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspace()
+        $runspace.Open(); $powershell.Runspace = $runspace
+        [ValidateNotNull()][System.Action]$_Action = $_Action;
+        Write-Host "Run In Background .." -ForegroundColor DarkBlue
+
+        $threads = New-Object System.Collections.ArrayList;
+        $result = [PSCustomObject]@{
+            PowerShell = $PowerShell
+            returnVal  = $PowerShell.BeginInvoke()
+        }
+        $threads.Add($result) | Out-Null;
+        $completed = $false;
+        while ($completed -eq $false) {
+            $completed = $true;
+            foreach ($thread in $threads) {
+                $endInvoke = $thread.PowerShell.EndInvoke($thread.returnVal);
+                $endInvoke;
+                $threadHandle = $thread.returnVal.AsyncWaitHandle.Handle;
+                $threadIsCompleted = $thread.returnVal.IsCompleted;
+                Write-Host "$threadHandle is $threadIsCompleted";
+                if ($threadIsCompleted -eq $false) {
+                    $completed = $false;
+                }
+            }
+            Write-Host "";
+            Start-Sleep -Milliseconds 500;
+        }
+        foreach ($thread in $threads) {
+            $thread.PowerShell.Dispose();
+        }
+        $_result = $result
+    }
+    end {
+        return $_result
+    }
+}
+# $t = New-Task { "running ...."; Start-Sleep -Seconds 3;  return "hello world" }
 
 Export-ModuleMember -Function '*' -Variable '*' -Cmdlet '*' -Alias '*' -Verbose:($VerbosePreference -eq "Continue")

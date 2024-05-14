581c4f89aec8663744009eef09e558fea8cb0eed
Alain Herve <alainQtec@gmail.com>
2024-05-14 17:47:35 +0200

    Create StackTracer.ps1

---
+++ Private/StackTracer.ps1
@@ -0,0 +1,59 @@
+﻿class StackTracer {
+    static [System.Collections.Concurrent.ConcurrentStack[string]]$stack = [System.Collections.Concurrent.ConcurrentStack[string]]::new()
+    static [System.Collections.Generic.List[hashtable]]$CallLog = @()
+    static [void] Push([type]$class) {
+        $str = "[{0}]" -f $class
+        if ([StackTracer]::Peek() -ne "$class") {
+            [StackTracer]::stack.Push($str)
+            $LAST_ERROR = $(Get-Variable -Name Error -ValueOnly)[0]
+            [StackTracer]::CallLog.Add(@{ ($str + ' @ ' + [datetime]::Now.ToShortTimeString()) = $(if ($null -ne $LAST_ERROR) { $LAST_ERROR.ScriptStackTrace } else { [System.Environment]::StackTrace }).Split("`n").Replace("at ", "# ").Trim() })
+        }
+    }
+    static [type] Pop() {
+        $result = $null
+        if ([StackTracer]::stack.TryPop([ref]$result)) {
+            return $result
+        } else {
+            throw [System.InvalidOperationException]::new("Stack is empty!")
+        }
+    }
+    static [string] Peek() {
+        $result = $null
+        if ([StackTracer]::stack.TryPeek([ref]$result)) {
+            return $result
+        } else {
+            return [string]::Empty
+        }
+    }
+    static [int] GetSize() {
+        return [StackTracer]::stack.Count
+    }
+    static [bool] IsEmpty() {
+        return [StackTracer]::stack.IsEmpty
+    }
+}
+function Pop-Stack {
+    [CmdletBinding()]
+    param ()
+    process {
+        return [StackTracer]::Pop()
+    }
+}
+
+function Push-Stack {
+    [CmdletBinding()]
+    param (
+        [type]$class
+    )
+    process {
+        [StackTracer]::Push($class)
+    }
+}
+
+function Show-Stack {
+    [CmdletBinding()]
+    param ()
+    process {
+        [StackTracer]::Peek()
+    }
+}

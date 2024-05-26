a40f94949d83c4e4d4314ae892de21856661699e
Alain Herve <alainQtec@gmail.com>
2024-05-27 00:35:08 +0200

    Update Connect-Vault.ps1

--- Public/Connect-Vault.ps1
+++ Public/Connect-Vault.ps1
@@ -14,8 +14,8 @@
         # Return the Vault object so that it can be consumed and verified by other cmdlets
         $VAULT_USER = $personalVault.UserName; [ValidateNotNullOrEmpty()][string]$VAULT_USER = $VAULT_USER
         $VAULT_PASS = $personalVault.Password | ConvertFrom-SecureString; [ValidateNotNullOrEmpty()][string]$VAULT_PASS = $VAULT_PASS
-        [System.Environment]::SetEnvironmentVariable("PERSONALVAULT_U", $VAULT_USER, [System.EnvironmentVariableTarget]::Process)
-        [System.Environment]::SetEnvironmentVariable("PERSONALVAULT_P", $VAULT_PASS, [System.EnvironmentVariableTarget]::Process)
+        [System.Environment]::SetEnvironmentVariable("ARGONCAGE_U", $VAULT_USER, [System.EnvironmentVariableTarget]::Process)
+        [System.Environment]::SetEnvironmentVariable("ARGONCAGE_P", $VAULT_PASS, [System.EnvironmentVariableTarget]::Process)
         return $personalVault
     }
 }

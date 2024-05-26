5f80c91f0048ed12af9ec9eed99f1dc540c9b0fe
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:32:20 +0200

    Create Import-Vault.ps1

---
+++ Public/Import-Vault.ps1
@@ -0,0 +1,22 @@
+﻿function Import-Vault {
+    [CmdletBinding()]
+    [Alias('Load-Vault')]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [securestring] $RecoveryWord
+    )
+    process {
+        if ([Vault]::GetConnection().IsValid) {
+            if ([Vault]::ValidateRecoveryWord($RecoveryWord)) {
+                $res = Import-Clixml -Path ([Vault]::GetConnectionFile())
+                return [PSCustomObject]@{
+                    UserName = $res.UserName
+                    Password = ([pscredential]::new("P", $res.Password)).GetNetworkCredential().Password
+                }
+            } else {
+                Write-Warning "Recovery word is incorrect. Please pass the valid recovery word and try again."
+            }
+        } else { [Vault]::Write_connectionWarning() }
+    }
+}

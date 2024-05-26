2a0b2b89a7cd0f6bd5dea3514157d9a13af085a7
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:31:50 +0200

    Create Connect-Vault.ps1

---
+++ Public/Connect-Vault.ps1
@@ -0,0 +1,21 @@
+﻿function Connect-Vault {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseOutputTypeCorrectly", "")]
+    [CmdletBinding()]
+    [Alias('Login-Vault', 'Access-Vault')]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [pscredential] $Credential
+    )
+    process {
+        $personalVault = [Vault]::new()
+        $personalVault.UserName = if ([string]::IsNullOrEmpty($Credential.UserName)) { [Vault]::GetUser() } else { $Credential.UserName }
+        $personalVault.Password = $Credential.Password
+        # Return the Vault object so that it can be consumed and verified by other cmdlets
+        $VAULT_USER = $personalVault.UserName; [ValidateNotNullOrEmpty()][string]$VAULT_USER = $VAULT_USER
+        $VAULT_PASS = $personalVault.Password | ConvertFrom-SecureString; [ValidateNotNullOrEmpty()][string]$VAULT_PASS = $VAULT_PASS
+        [System.Environment]::SetEnvironmentVariable("PERSONALVAULT_U", $VAULT_USER, [System.EnvironmentVariableTarget]::Process)
+        [System.Environment]::SetEnvironmentVariable("PERSONALVAULT_P", $VAULT_PASS, [System.EnvironmentVariableTarget]::Process)
+        return $personalVault
+    }
+}

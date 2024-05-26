52d32208e4a2011b90fca158ac4d3cbe743a6ad8
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:31:54 +0200

    Create Disconnect-Vault.ps1

---
+++ Public/Disconnect-Vault.ps1
@@ -0,0 +1,13 @@
+﻿function Disconnect-Vault {
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+    param (
+        [switch] $Force
+    )
+    process {
+        if ([Vault]::GetConnection().IsValid) {
+            if ($Force.IsPresent -or $PSCmdlet.ShouldProcess("Connection", "Disconnect Vault")) {
+                [Vault]::ClearConnection()
+            }
+        } else { [Vault]::Write_connectionWarning() }
+    }
+}

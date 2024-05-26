6bde9a4a3db81e43c015c4c25d4e35d00d5ad470
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:32:05 +0200

    Create Get-Key.ps1

---
+++ Public/Get-Key.ps1
@@ -0,0 +1,25 @@
+﻿function Get-Key {
+    [CmdletBinding()]
+    [Alias('Fetch-Key')]
+    [OutputType([string])]
+    param (
+        [switch] $Force
+    )
+    process {
+        if ([Vault]::GetConnection().IsValid) {
+            if ([IO.File]::Exists([Vault]::GetKeyFile())) {
+                $res = Import-Clixml ([Vault]::GetKeyFile())
+                $key = [pscredential]::new("key", $res)
+                $key = $key.GetNetworkCredential().Password
+            }
+            if (![IO.File]::Exists([Vault]::GetKeyFile())) {
+                $key = [vault]::GenerateKey(); [Vault]::SaveKey($key, $false)
+            }
+            if ($Force.IsPresent) {
+                [Vault]::ArchiveKeyFile()
+                $key = [vault]::GenerateKey(); [Vault]::SaveKey($key, $true)
+            }
+            return $key
+        } else { [Vault]::Write_connectionWarning() }
+    }
+}

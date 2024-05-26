02d65177210ded7a3f0e3d6cb306e5f65c4deaea
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:32:26 +0200

    Create Remove-Vault.ps1

---
+++ Public/Remove-Vault.ps1
@@ -0,0 +1,14 @@
+﻿function Remove-Vault {
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+    [Alias('Delete-Vault')]
+    param (
+        [switch] $Force
+    )
+    process {
+        if ([Vault]::GetConnection().IsValid) {
+            if ($Force.IsPresent -or $PSCmdlet.ShouldProcess("Personal Vault", "Remove-PSPersonalVault")) {
+                [Vault]::ClearDb()
+            }
+        } else { [Vault]::Write_connectionWarning() }
+    }
+}

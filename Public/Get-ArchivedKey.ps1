1d96673994b34ab304ce327cdfd1c3488a9ee04c
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:31:59 +0200

    Create Get-ArchivedKey.ps1

---
+++ Public/Get-ArchivedKey.ps1
@@ -0,0 +1,32 @@
+﻿function Get-ArchivedKey {
+    [CmdletBinding()]
+    [Alias('Fetch-ArchivedKey')]
+    [OutputType([object[]])]
+    param (
+        [Parameter(Position = 0, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [datetime] $DateModified
+    )
+    process {
+        if ([Vault]::GetConnection().IsValid) {
+            $archivePath = Join-Path -Path (Split-Path -Path ([Vault]::GetKeyFile()) -Parent) -ChildPath "archive"
+            if (Test-Path $archivePath) {
+                $results = @()
+                $archivedFiles = Get-ChildItem -Path $archivePath | Select-Object FullName, LastWriteTime
+                if ($PSBoundParameters.ContainsKey('DateModified')) {
+                    $archivedFiles = $archivedFiles | Where-Object { (Get-Date $_.LastWriteTime -Format ddMMyyyy) -eq (Get-Date $DateModified -Format ddMMyyyy) }
+                }
+                $archivedFiles | ForEach-Object {
+                    $key = Import-Clixml $_.FullName
+                    $keyObj = [pscredential]::new("key", $key)
+                    $obj = [PSCustomObject]@{
+                        DateModified = $_.LastWriteTime
+                        Key          = $keyObj.GetNetworkCredential().Password
+                    }
+                    $results += $obj
+                }
+                return $results
+            }
+        } else { [Vault]::Write_connectionWarning() }
+    }
+}

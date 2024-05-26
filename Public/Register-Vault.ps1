adb3c90f45b2b3fee6cbbe7a6e2344575c31102f
Alain Herve <alainQtec@gmail.com>
2024-05-26 23:32:47 +0200

    Create Register-Vault.ps1

---
+++ Public/Register-Vault.ps1
@@ -0,0 +1,24 @@
+﻿function Register-Vault {
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [pscredential] $Credential,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [securestring] $RecoveryWord,
+        [switch] $Force
+    )
+    process {
+        $personalVault = [Vault]::new()
+        $personalVault.UserName = if ([string]::IsNullOrEmpty($Credential.UserName)) { [Vault]::GetUser() } else { $Credential.UserName }
+        $personalVault.Password = $Credential.Password
+        $personalVault.Key = $RecoveryWord
+        if (!(Test-Path ([Vault]::GetConnectionFile()))) { $personalVault | Export-Clixml -Path ([Vault]::GetConnectionFile()) }
+        if ($Force.IsPresent) {
+            if ([Vault]::GetConnection().IsValid) {
+                $personalVault | Export-Clixml -Path ([Vault]::GetConnectionFile()) -Force
+            } else { [Vault]::Write_connectionWarning() }
+        }
+    }
+}

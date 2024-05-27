7ab14c9dfd53fcb2904851d5e61b0d590a9f489e
Alain Herve <alainQtec@gmail.com>
2024-05-28 01:25:59 +0200

    Update Import-Vault.ps1

--- Public/Import-Vault.ps1
+++ Public/Import-Vault.ps1
@@ -1,6 +1,5 @@
 ﻿function Import-Vault {
     [CmdletBinding()]
-    [Alias('LoadVault')]
     param (
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
@@ -9,7 +8,7 @@
     process {
         if ([ArgonCage]::vault.GetConnection().IsValid) {
             if ([ArgonCage]::vault.validateRecoveryWord($RecoveryWord)) {
-                $res = Import-Clixml -Path ([ArgonCage]::vault.GetConnectionFile())
+                $res = Import-Clixml -Path ([ArgonCage]::vault.ConnectionFile)
                 return [PSCustomObject]@{
                     UserName = $res.UserName
                     Password = ([pscredential]::new("P", $res.Password)).GetNetworkCredential().Password
@@ -17,6 +16,6 @@
             } else {
                 Write-Warning "Recovery word is incorrect. Please pass the valid recovery word and try again."
             }
-        } else { [ArgonCage]::vault.write_connectionWarning() }
+        } else { Write-Warning -Message [Vault].MSG.CONNECTION_WARNING }
     }
 }

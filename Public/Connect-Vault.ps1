c1ab425c5a408d7862b63e3de4e94d9c113b29eb
Alain Herve <alainQtec@gmail.com>
2024-05-28 01:25:30 +0200

    Update Connect-Vault.ps1

--- Public/Connect-Vault.ps1
+++ Public/Connect-Vault.ps1
@@ -1,21 +1,20 @@
 ﻿function Connect-Vault {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseOutputTypeCorrectly", "")]
     [CmdletBinding()]
-    [Alias('Login-Vault', 'Access-Vault')]
+    [Alias('Access-Vault')]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [pscredential] $Credential
     )
     process {
-        $personalVault = [ArgonCage]::vault
-        $personalVault.UserName = if ([string]::IsNullOrEmpty($Credential.UserName)) { [ArgonCage]::vault.GetUser() } else { $Credential.UserName }
-        $personalVault.Password = $Credential.Password
+        $vaultSession = [ArgonCage]::GetVault()
+        $vaultSession.UserName = if ([string]::IsNullOrEmpty($Credential.UserName)) { [ArgonCage]::vault.UserName } else { $Credential.UserName }
+        $vaultSession.Password = $Credential.Password
         # Return the Vault object so that it can be consumed and verified by other cmdlets
-        $VAULT_USER = $personalVault.UserName; [ValidateNotNullOrEmpty()][string]$VAULT_USER = $VAULT_USER
-        $VAULT_PASS = $personalVault.Password | ConvertFrom-SecureString; [ValidateNotNullOrEmpty()][string]$VAULT_PASS = $VAULT_PASS
+        $VAULT_USER = $vaultSession.UserName; [ValidateNotNullOrEmpty()][string]$VAULT_USER = $VAULT_USER
+        $VAULT_PASS = $vaultSession.Password | ConvertFrom-SecureString; [ValidateNotNullOrEmpty()][string]$VAULT_PASS = $VAULT_PASS
         [System.Environment]::SetEnvironmentVariable("ARGONCAGE_U", $VAULT_USER, [System.EnvironmentVariableTarget]::Process)
         [System.Environment]::SetEnvironmentVariable("ARGONCAGE_P", $VAULT_PASS, [System.EnvironmentVariableTarget]::Process)
-        return $personalVault
+        return $vaultSession
     }
 }

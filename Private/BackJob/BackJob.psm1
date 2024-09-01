fe6d80f04bd3944e46cb4df0f30075112af717ff
Alain Herve <alainQtec@gmail.com>
2024-09-01 15:38:20 +0200

    Update BackJob.psm1

--- Private/BackJob/BackJob.psm1
+++ Private/BackJob/BackJob.psm1
@@ -1,4 +1,7 @@
-﻿
+﻿using namespace System.IO
+using namespace System.Collections.Generic
+using namespace System.Management.Automation
+
 # .SYNOPSIS
 # BackJobs.psm1 Contains classes & functions to help with Background jobs
 
@@ -182,6 +185,256 @@ class StackTracer {
         return [StackTracer]::stack.IsEmpty
     }
 }
+
+#region    cboxclasses
+enum HostOs {
+    Windows
+    Linux
+    MacOS
+    UNKNOWN
+}
+class MinRequirements {
+    [int]$FreeMemGB = 1
+    [int]$MinDiskGB = 10
+    [bool]$RunAsAdmin = $false
+    [FileInfo[]]$RequiredFiles
+    [DirectoryInfo[]]$RequiredDirectories
+    hidden [string[]]$IgnoredProps = @()
+    MinRequirements() {}
+}
+class InstallReport {
+    hidden [string]$Title
+    InstallReport ([string]$Title, [hashtable]$table) {
+        $this.Title = $Title; $this.SetObjects($table)
+    }
+    hidden [void] SetObjects([hashtable]$table) {
+        $dict = [System.Collections.Generic.Dictionary[string, string]]::new([System.StringComparer]::OrdinalIgnoreCase)
+        $table.Keys.ForEach({ [void]$dict.Add($_, $table[$_]) }); $this.SetObjects($dict)
+    }
+    [void] SetObjects([System.Collections.Generic.Dictionary[string, string]]$dict) {
+        $dict.Keys.ForEach({ $this.psobject.Properties.Add([PSScriptProperty]::new($_, [scriptblock]::Create("return '$($dict[$_])'"), { throw "$_ is a readonly property" })) })
+    }
+    [string] ToString() {
+        return (" " + $this.Title + "`n" + ($this | Format-List | Out-String))
+    }
+}
+
+class SetupStep {
+    [string]$MethodName
+    [string]$Desscription
+    [System.Array]$ArgumentList
+
+    SetupStep([string]$Name) {
+        $this.MethodName = $Name
+    }
+    SetupStep([string]$Name, [System.Array]$ArgumentList) {
+        $this.MethodName = $Name; $this.ArgumentList = $ArgumentList
+    }
+    [SetupStep] SetDescription([string]$Desscription) {
+        $this.Desscription = $Desscription
+        return $this
+    }
+}
+
+class Setup {
+    [string] $Name
+    [bool] $RunAsAdmin = $true
+    static [HostOs] $HostOs = [Setup]::GetHostOs()
+    static [ActionPreference] $OnError = (Get-Variable -Name ErrorActionPreference -ValueOnly)
+    [System.Collections.Generic.Queue[SetupStep]]$Steps = @()
+    Setup() {}
+    [TaskResult] Run([Setup]$setup) {
+        return $this.Run($setup, $true)
+    }
+    [TaskResult] Run([SetupStep]$step) {
+        return $this.Run($step, $false)
+    }
+    [TaskResult] Run([Setup]$setup, [bool]$Async) {
+        $result = [TaskResult]::new()
+        # if (!$setup.CheckRequirements()) { throw "Minimum requirements were not met" }
+        $c = 1; $setup.Steps.ForEach({
+                Write-Verbose "[$c/$($setup.Steps.Count)] $($_.MethodName) ..."
+                [void]$result.Output.Add($setup.Run($_, $Async))
+                $c++
+            }
+        )
+        return $result
+    }
+    [TaskResult] Run([SetupStep]$step, [bool]$Async) {
+        if ($Async) {
+            return [TaskResult]::new($(Start-Job -Name $step.MethodName -ScriptBlock {
+                        param($setup, $stp)
+                        if (0 -eq $stp.ArgumentList.Count) { return $setup."$($stp.MethodName)"() }
+                        return $setup."$($stp.MethodName)"($stp.ArgumentList)
+                    } -ArgumentList $this, $step
+                )
+            )
+        } else {
+            if (0 -eq $step.ArgumentList.Count) { return [TaskResult]::new($this."$($step.MethodName)"()) }
+            return [TaskResult]::new($this."$($step.MethodName)"($step.ArgumentList))
+        }
+    }
+    static [Ordered] CheckRequirements([MinRequirements]$InstallReqs) {
+        if (!$InstallReqs.RunAsAdmin) { $InstallReqs.IgnoredProps += "HasAdminPrivileges" }
+        $h = @{
+            HasEnoughRAM          = $InstallReqs.FreeMemGB -le [Setup]::GetfreRAMsize()
+            HasEnoughDiskSpace    = $InstallReqs.MinDiskGB -le [math]::Round((Get-PSDrive -Name ([IO.Directory]::GetDirectoryRoot((Get-Location)))).Free / 1GB)
+            HasAdminPrivileges    = $InstallReqs.RunAsAdmin -and [Setup]::IsAdmin()
+            HasAllRequiredFiles   = $InstallReqs.RequiredFiles.Where({ ![IO.File]::Exists($_.FullName) }).count -eq 0
+            HasAllRequiredFolders = $InstallReqs.RequiredDirectories.Where({ ![IO.Directory]::Exists($_.FullName) }).count -eq 0
+        }
+        Write-Verbose -Message $("Checking install requirements ...`n{0}" -f (New-Object PsObject -Property $h | Out-String).TrimEnd())
+        $r = [Ordered]::new(); $h.Keys.Where({ $_ -notin $InstallReqs.IgnoredProps }).ForEach({ $r.Add($_, $h[$_]) })
+        return $r
+    }
+    static [HostOs] GetHostOs() {
+        return $(
+            if ($(Get-Variable IsWindows -Value)) {
+                "Windows"
+            } elseif ($(Get-Variable IsLinux -Value)) {
+                "Linux"
+            } elseif ($(Get-Variable IsMacOS -Value)) {
+                "MacOS"
+            } else {
+                "UNKNOWN"
+            }
+        )
+    }
+    static [bool] IsAdmin() {
+        [string]$_Host_OS = [Setup]::getHostOs()
+        [bool]$Isadmin = $(switch ($true) {
+                $($_Host_OS -eq "Windows") {
+                    $(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator);
+                    break;
+                }
+                $($_Host_OS -in ("MacOS", "Linux")) {
+                    $(whoami) -eq "root";
+                    break;
+                }
+                Default {
+                    Write-Warning "Unknown OS: $_Host_OS";
+                    $false
+                }
+            }
+        )
+        if (!$Isadmin) { Write-Warning "[USER is not ADMIN]. This script requires administrative privileges." }
+        return $Isadmin
+    }
+    static [int] GetfreRAMsize() {
+        [string]$OsName = [Setup]::GetHostOs();
+        return $(switch ($OsName) {
+                "Windows" {
+                    [math]::Round((Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1GB)
+                    break
+                }
+                "Linux" {
+                    [math]::Round([int64](((& free -b) -split "`n")[1] -split "\s+")[1] / 1GB, 2)
+                    break;
+                }
+                "MacOs" {
+                    [math]::Round(((& sysctl hw.memsize) -split ' ')[1] / 1GB, 2)
+                    break;
+                }
+                Default { throw "Unable to read memory size for OS: $OsName" }
+            }
+        )
+    }
+    hidden [void] SetSteps([hashtable]$method_args_hash) {
+        $method_args_hash.Keys.ForEach({ $this.steps.Enqueue([SetupStep]::New($_, $method_args_hash[$_])) })
+    }
+    [InstallReport] GetInstallReport() {
+        return [InstallReport]::new("$($this.Name) setup completed successfully.")
+    }
+}
+
+class CboxSetup : Setup {
+    [bool]$UseCloudVps = $false
+    CboxSetup() {
+        $this.Name = "C/C++ development environment"
+        $this.SetSteps(@{
+                CheckRequirements        = @()
+                InstallChoco             = @()
+                InstallMSYS2             = @()
+                InstallGcc               = @()
+                InstallClang             = @()
+                InstallCMake             = @()
+                PrepareVisualStudioCode  = @()
+                InstallGoogleTestPackage = @()
+                InstallDoxygen           = @()
+                InstallGit               = @()
+            }
+        )
+    }
+    [TaskResult] Run() {
+        if ($this.UseCloudVps -and $this.RunAsAdmin) {
+            throw [System.InvalidOperationException]::new("Cannot run as admin on a cloud VPS")
+        }
+        Write-Verbose "Running setup: $($this.Name) ..."
+        return $this.Run($this, $false)
+    }
+    [void] InstallChoco($nptargs) {
+        # Install Chocolatey package manager
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
+    [void] InstallMSYS2($nptargs) {
+        # Install MSYS2
+        choco install msys2 --params = "/NoUpdate" -y
+
+        # Add MSYS2 binaries to system PATH
+        $msys2BinPath = Join-Path $env:ChocolateyInstall "lib\msys2\mingw64\bin"
+        $env:Path += ";$msys2BinPath"
+        [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    }
+    [void] InstallGcc($nptargs) {
+        # Install GCC and related tools
+        & "$env:ChocolateyInstall\lib\msys2\tools\msys2.exe" -c "/usr/bin/bash -lc 'pacman -Syu --noconfirm'" | Out-Null
+        & "$env:ChocolateyInstall\lib\msys2\tools\msys2.exe" -c "/usr/bin/bash -lc 'pacman -S --needed --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-make mingw-w64-x86_64-gdb'" | Out-Null
+    }
+    [void] InstallClang($nptargs) {
+        # Install Clang
+        & "$env:ChocolateyInstall\lib\msys2\tools\msys2.exe" -c "/usr/bin/bash -lc 'pacman -S --needed --noconfirm mingw-w64-x86_64-clang mingw-w64-x86_64-clang-tools-extra'" | Out-Null
+    }
+    [void] InstallCMake($nptargs) {
+        # Install CMake
+        choco install cmake --install-arguments = 'ADD_CMAKE_TO_PATH=System' -y
+    }
+    [void] PrepareVisualStudioCode($nptargs) {
+        choco install vscode -y
+        # Install C/C++ extension for VSCode
+        code --install-extension ms-vscode.cpptools
+
+        # Install CMake Tools extension for VSCode
+        code --install-extension ms-vscode.cmake-tools
+    }
+    [void] InstallGoogleTestPackage($nptargs) {
+        # Install Google Test package
+        choco install vcpkg -y
+        & "$env:ChocolateyInstall\lib\vcpkg\tools\vcpkg.exe" integrate install
+        & "$env:ChocolateyInstall\lib\vcpkg\tools\vcpkg.exe" install gtest:x64-windows
+    }
+    [void] InstallDoxygen($nptargs) {
+        choco install doxygen.install -y
+    }
+    [void] InstallGit($nptargs) {
+        choco install git -y
+    }
+    [bool] CheckRequirements() {
+        return $this.CheckRequirements(@())
+    }
+    [bool] CheckRequirements($nptargs) {
+        $InstallReqs = [MinRequirements]::new();
+        $InstallReqs.RunAsAdmin = $this.RunAsAdmin;
+        $InstallReqs.RequiredFiles += "$env:ChocolateyInstall\bin\choco.exe"
+        $InstallReqs.RequiredFiles += "$env:ChocolateyInstall\lib\msys2\tools\msys2.exe"
+        $InstallReqs.RequiredDirectories += "$env:ChocolateyInstall\bin"
+        $InstallReqs.RequiredDirectories += "$env:ChocolateyInstall\lib\msys2\tools"
+        return [CboxSetup]::CheckRequirements($InstallReqs).Values -notcontains $false
+    }
+}
+#endregion cboxclasses
+
 function Pop-Stack {
     [CmdletBinding()]
     param ()
@@ -508,51 +761,58 @@ function BackgroundTask {
     )
 
     begin {
-        class TaskCommands : System.Collections.Generic.List[String] {
-            TaskCommands() {}
-            [string] ToString() {
-                if ($this.Count -eq 0) { return [string]::Empty }else { return '...' }
-            }
-        }
         class TaskResult {
-            [bool]$IsSuccess
-            [string]$JobName
-            [array]$ErrorRecord
-            hidden [TaskCommands]$Commands
-            [System.Management.Automation.PSDataCollection[psobject]]$Output
-            TaskResult() {
-                $this.IsSuccess = $false
-                $this.JobName = [string]::Empty
-                $this.ErrorRecord = @()
-                $this.Commands = [TaskCommands]::new()
-                $this.Output = [System.Management.Automation.PSDataCollection[psobject]]::new()
+            # .EXAMPLE
+            #     New-Object TaskResult((Start-Job -ScriptBlock { start-sleep -seconds 2; return 100 }))
+            [bool]$IsSuccess = $false
+            [string]$JobName = [string]::Empty
+            [System.Array]$ErrorRecord = @()
+            hidden [System.Collections.Generic.List[String]]$Commands = @()
+            [PSDataCollection[psobject]]$Output = [PSDataCollection[psobject]]::new()
+            TaskResult() { $this.SetJobState() }
+            TaskResult($InputObject) {
+                $t = [TaskResult]::Create($InputObject)
+                $this.PSObject.Properties.Name.ForEach({ $this."$_" = $t."$_" }); $this.SetJobState()
             }
-            [void] SetJobState() { $this.SetJobState($null) }
-            [void] SetJobState([scriptblock]$get_state) {
-                # .EXAMPLE
-                #   $result.SetJobState()
-                # .EXAMPLE
-                #   $result.SetJobState({ return 'StateSTR' })
-                if ($null -eq $get_state) {
-                    $get_state = [scriptblock]::Create({
-                            return $(switch ($true) {
-                                    $(![string]::IsNullOrWhiteSpace($this.JobName)) {
-                                        $(Get-Job -Name $this.JobName).State.ToString();
-                                        break;
-                                    }
-                                    $($this.IsSuccess) {
-                                        "Completed";
-                                        break;
-                                    }
-                                    Default {
-                                        "Failed"
-                                    }
-                                }
-                            )
-                        }
-                    )
+            TaskResult($InputObject, [ErrorRecord]$ErrorRecord) {
+                $t = [TaskResult]::Create($InputObject, $ErrorRecord)
+                $this.PSObject.Properties.Name.ForEach({ $this."$_" = $t."$_" }); $this.SetJobState()
+            }
+            TaskResult($InputObject, [bool]$IsSuccess, [ErrorRecord]$ErrorRecord) {
+                $t = [TaskResult]::Create($InputObject, $IsSuccess, $ErrorRecord)
+                $this.PSObject.Properties.Name.ForEach({ $this."$_" = $t."$_" }); $this.SetJobState()
+            }
+            static [TaskResult] Create($InputObject) {
+                if ($InputObject -is [array]) {
+                    Write-Verbose "InputObject is an array"
+                    $_Params = [hashtable]::new()
+                    $_Params.IsSuccess = $InputObject.Where({ $_ -is [bool] });
+                    $_Params.ErrorRecord = $InputObject.Where({ $_ -is [ErrorRecord] });
+                    $_Params.InputObject = $InputObject[0];
+                    return [TaskResult]::Create($_Params.InputObject, $_Params.IsSuccess, $_Params.ErrorRecord)
                 }
-                $this.PsObject.Properties.Add([psscriptproperty]::new('State', $get_state, { throw [System.InvalidOperationException]::new("Cannot set State") }))
+                return [TaskResult]::Create($InputObject, $false, $null)
+            }
+            static [TaskResult] Create($InputObject, [ErrorRecord]$ErrorRecord) {
+                return [TaskResult]::Create($InputObject, $false, $ErrorRecord)
+            }
+            static [TaskResult] Create($InputObject, [bool]$IsSuccess, [ErrorRecord]$ErrorRecord) {
+                $tresult = [TaskResult]::new(); $err = $null; if ($null -eq $InputObject) { $InputObject = [PSObject]::new() }
+                if ($null -ne $ErrorRecord) { $tresult.ErrorRecord += $ErrorRecord };
+                if ($InputObject -is [job]) {
+                    $tresult.JobName = $InputObject.Name;
+                    $tresult.Commands.Add($InputObject.Command) | Out-Null
+                    $InputObject = $InputObject.ChildJobs | Receive-Job -Wait -ErrorAction SilentlyContinue -ErrorVariable Err
+                    $tresult.IsSuccess = $null -eq $Err; if (!$tresult.IsSuccess) { $tresult.ErrorRecord += $err }
+                    if ($InputObject -is [bool]) { $tresult.IsSuccess = $InputObject }
+                }
+                $tresult.Output.Add($InputObject) | Out-Null
+                $tresult.SetJobState()
+                $tresult.IsSuccess = $tresult.ErrorRecord.Count -eq 0 -and $($tresult.State -ne "Failed")
+                return $tresult
+            }
+            [void] SetJobState() {
+                $this.PsObject.Properties.Add([psscriptproperty]::new('State', { return $(switch ($true) { $(![string]::IsNullOrWhiteSpace($this.JobName)) { $(Get-Job -Name $this.JobName).State.ToString(); break } $($this.IsSuccess) { "Completed"; break } Default { "Failed" } }) }, { throw [System.InvalidOperationException]::new("Cannot set State") }))
             }
         }
         $_BoundParams = [hashtable]$PSCmdlet.MyInvocation.BoundParameters
